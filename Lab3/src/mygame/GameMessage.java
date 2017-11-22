48f7e79ff28b0663bae9b47c08e4b039d56db900
NoExitTV <fredrikopettersson@hotmail.com>
2017-11-22 12:08:46 +0100

    Thinking about how to implement lab3

--- Lab3/src/mygame/GameMessage.java
+++ Lab3/src/mygame/GameMessage.java
@@ -1,5 +1,9 @@
 package mygame;
 
+import com.jme3.network.AbstractMessage;
+import com.jme3.network.serializing.Serializable;
+import com.jme3.network.serializing.Serializer;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,8 +12,34 @@ package mygame;
 
 /**
  *
- * @author NoExit
+ * @author Fredrik Pettersosn
  */
 public class GameMessage {
+    public static void initSerializer() {
+        Serializer.registerClass(GameMessage.class);
+    }
     
+    private abstract class message extends AbstractMessage {
+        
+    }
+    
+    // =======================================================================
+    // CLIENT -> SERVER
+    // =======================================================================
+    
+    @Serializable
+    public static class connectMessage extends AbstractMessage {
+        
+        String nickname;
+        String client;
+        //Do stuff here lixx....
+        public connectMessage(String n, String client) {
+            this.nickname = n;
+            this.client = client;
+        }
+    }
+    
+    // =======================================================================
+    // SERVER -> CLIENT
+    // =======================================================================
 }
